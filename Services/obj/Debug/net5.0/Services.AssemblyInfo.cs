a0d611b3993157314521befaf597c21ef2aacf6e
harunkirtay <harun.kirtay@Profen.com>
2024-12-03 17:35:38 +0300

    asdfghj

--- Services/obj/Debug/net5.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net5.0/Services.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25b009c6cfa4670a9aa7fbf270b2877067c3f205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+159d4c2cd4f440e069784ac8add313a3631586c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
