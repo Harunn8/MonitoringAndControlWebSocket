defaa292c12612071196f3261c4918609e9219c8
MSI\harun <harunkirtau@gmail.com>
2024-12-29 00:34:34 +0300

    edit assembly and dll

--- Services/obj/Debug/net5.0/Services.AssemblyInfo.cs
+++ Services/obj/Debug/net5.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7858bc7d8f3550827dd7a7b0d17026e08d380720")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5afd525ba8dad61859f9e6f8148299c1ec1cb7c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
